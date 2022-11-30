df66f2bf99d7b9adb371ea6eddc946b39a15c204
haseeb-official <haseebpp.official@gmail.com>
2022-12-01 00:22:20 +0530

    Preview order started

---
+++ frontend/src/components/PreviewOrder.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const PreviewOrder = () => {
+  return (
+    <div class="flex w-full flex-col items-center gap-10 md:rounded-lg bg-white p-12 shadow-2xl shadow-gray-300">
+      <h1>Preview Order</h1>
+    </div>
+  )
+}
+
+export default PreviewOrder
--- frontend/src/nav-tabs/Work.jsx
+++ frontend/src/nav-tabs/Work.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux"
 import AddCustomer from "../components/AddCustomer"
 import OrderGarment from "../components/OrderGarment"
 import MeasureOrder from "../components/MeasureOrder"
+import PreviewOrder from "../components/PreviewOrder"
 
 export default function Work() {
   const dispatch = useDispatch()
@@ -45,10 +46,18 @@ export default function Work() {
           } rounded-full`}
           onClick={() => setTab("measure")}
         ></div>
+        <div class="h-1 w-20  bg-gray-300 rounded-full"></div>
+        <div
+          class={`${
+            tab === "preview" ? "h-8 w-8  bg-green-500" : "h-6 w-6  bg-gray-300"
+          } rounded-full`}
+          onClick={() => setTab("preview")}
+        ></div>
       </div>
       {tab === "add" && <AddCustomer />}
       {tab === "order" && <OrderGarment setTab={setTab} />}
       {tab === "measure" && <MeasureOrder setTab={setTab} />}
+      {tab === "preview" && <PreviewOrder setTab={setTab} />}
     </>
   )
 }
